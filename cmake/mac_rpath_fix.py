887d70e35fa53d46d81fd0780dbfd01af1f4bb7c
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-09-04 23:20:34 +0300

    Fix build on Mac OS X

--- cmake/mac_rpath_fix.py
+++ cmake/mac_rpath_fix.py
@@ -51,7 +51,7 @@ for f in files:
         lib_path = os.path.realpath(os.path.join(f, lib_name))
         for rpath in lib_rpaths:
             run(('install_name_tool', '-change', rpath, '@rpath/' + rpath, lib_path))
-        run(('install_name_tool', '-change', '/usr/lib/libz.1.dylib', '@rpath/zlib.framework/Versions/1/zlib', lib_path))
+#        run(('install_name_tool', '-change', '/usr/lib/libz.1.dylib', '@rpath/zlib.framework/Versions/1/zlib', lib_path))
 
 # plugins
 qt_install_plg_path = os.path.join(qt_path, 'plugins')
@@ -63,4 +63,4 @@ for f in files:
         run(('install_name_tool', '-add_rpath', '@loader_path/../../../Frameworks/', f)) #/plugins/4/crypto
         for rpath in lib_rpaths:
             run(('install_name_tool', '-change', rpath, '@rpath/' + rpath, f))
-        run(('install_name_tool', '-change', '/usr/lib/libz.1.dylib', '@rpath/zlib.framework/Versions/1/zlib', f))
+#        run(('install_name_tool', '-change', '/usr/lib/libz.1.dylib', '@rpath/zlib.framework/Versions/1/zlib', f))
